e8343b567f521ca4cf653a2aa99d1a929b121e7a
rychouwei <rychouwei@tencent.com>
2019-08-17 00:54:57 +0800

    docs: 完善Readme

--- src/App.js
+++ src/App.js
@@ -3,30 +3,31 @@ import './App.css';
 import iconurl from './icon/爱心.png'
 import Main from './Main'
 class App extends Component {
-    state={
-        class:'bounceInLeft',
-        show:false
-    };
+  state = {
+    class: 'bounceInLeft',
+    show: false,
+
+  };
 
   render() {
 
-        const main = ()=>{
-            if (this.state.show)
-                return <Main/>
-        }
-        return (
-          <div>
-              <div className={"envelope animated "+ this.state.class}>
-                <div className="triangle-down"></div>
-                  <img className="heart" src={iconurl} onClick={()=>{
-                      this.setState({class:'bounceOutRight',show:true})
-                  }}/>
-                  <div className="triangle-up"></div>
-                  <div className="text">你收到一封信，点击查收！</div>
-              </div>
-              {main()}
-          </div>
-        );
+    const main = () => {
+      if (this.state.show)
+        return <Main />
+    }
+    return (
+      <div>
+        <div className={"envelope animated " + this.state.class}>
+          <div className="triangle-down"></div>
+          <img className="heart" src={iconurl} onClick={() => {
+            this.setState({ class: 'bounceOutRight', show: true })
+          }} />
+          <div className="triangle-up"></div>
+          <div className="text">你收到一封信，点击查收！</div>
+        </div>
+        {main()}
+      </div>
+    );
   }
 }
 
